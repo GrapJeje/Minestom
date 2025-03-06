a950111a8eb56f72b7ba877473d27201e6284544
Jason <jason@camonetwork.eu>
2025-03-06 18:05:03 +0100

    Added a better cooldown system

--- src/main/java/nl/grapjeje/minestom/Listeners/Player/PlayerEntityInteractListener.java
+++ src/main/java/nl/grapjeje/minestom/Listeners/Player/PlayerEntityInteractListener.java
@@ -1,45 +1,28 @@
 package nl.grapjeje.minestom.Listeners.Player;
 
 import net.kyori.adventure.text.format.TextColor;
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.Player;
+import net.minestom.server.entity.*;
 import net.minestom.server.event.player.PlayerEntityInteractEvent;
-import net.minestom.server.timer.Scheduler;
-import net.minestom.server.timer.TaskSchedule;
-import net.minestom.server.utils.time.TimeUnit;
 import nl.grapjeje.minestom.Listeners.EventListener;
 import nl.grapjeje.minestom.Model.Ball;
 import nl.grapjeje.minestom.Util.Text;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class PlayerEntityInteractListener implements EventListener<PlayerEntityInteractEvent> {
-    private static final List<Player> playersOnCooldown = new ArrayList<>();
 
     @Override
     public void run(PlayerEntityInteractEvent e) {
         Player player = e.getPlayer();
         Entity entity = e.getTarget();
 
-        if (playersOnCooldown.contains(player)) return;
+        if (Ball.Cooldown.isPlayerOnCooldown(player)) return;
         if (entity.getEntityType() != EntityType.FALLING_BLOCK) return;
 
         Ball.instance.kick(player, this.getKickPower(player));
         player.sendMessage(Text.getColoredMessage(TextColor.fromHexString("#1FC077"), "Bal is getrapt! (Power: ")
                 .append(Text.getColoredMessage(TextColor.fromHexString("#D48341"), String.valueOf(this.getKickPower(player)))
                         .append(Text.getColoredMessage(TextColor.fromHexString("#1FC077"), ")"))));
-
-        var scheduler = MinecraftServer.getSchedulerManager();
-
-        playersOnCooldown.add(player);
-
-        scheduler.buildTask(() ->
-                playersOnCooldown.remove(player))
-                .delay(10, TimeUnit.SERVER_TICK).schedule();
     }
 
     private float getKickPower(Player player) {
--- src/main/java/nl/grapjeje/minestom/Model/Ball.java
+++ src/main/java/nl/grapjeje/minestom/Model/Ball.java
@@ -1,5 +1,6 @@
 package nl.grapjeje.minestom.Model;
 
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.collision.BoundingBox;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
@@ -9,11 +10,15 @@ import net.minestom.server.entity.metadata.display.BlockDisplayMeta;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.entity.Player;
+import net.minestom.server.utils.time.TimeUnit;
 import nl.grapjeje.minestom.Server;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ball {
     public static Ball instance;
-
     private Entity blockDisplayEntity;
 
     public void spawn(Pos pos) {
@@ -24,13 +29,10 @@ public class Ball {
 
         BlockDisplayMeta meta = (BlockDisplayMeta) blockDisplayEntity.getEntityMeta();
         meta.setBlockState(Block.STONE);
-        blockDisplayEntity.setBoundingBox(new BoundingBox(100, 100, 100));
-        blockDisplayEntity.setCustomNameVisible(true);
     }
 
-    public void kick(Player player, float power) {
-        if (power > 10) power = 10;
-        if (power < 0) power = 0;
+    public void kick(@NotNull Player player, float power) {
+        power = Math.max(0, Math.min(power, 10));
 
         Vec direction = player.getPosition().direction();
         direction = new Vec(direction.x(), 0, direction.z()).normalize();
@@ -39,7 +41,31 @@ public class Ball {
         float verticalForce = power * 0.8f;
 
         Vec force = new Vec(direction.x() * horizontalForce, verticalForce, direction.z() * horizontalForce);
-
         blockDisplayEntity.setVelocity(force);
+
+        Cooldown.addCooldown(player);
+    }
+
+    public static class Cooldown {
+        private static final Set<Player> playersOnCooldown = new HashSet<>();
+
+        public static void addCooldown(Player player) {
+            playersOnCooldown.add(player);
+            scheduleCooldownRemoval(player);
+        }
+
+        public static void removeCooldown(Player player) {
+            playersOnCooldown.remove(player);
+        }
+
+        public static boolean isPlayerOnCooldown(Player player) {
+            return playersOnCooldown.contains(player);
+        }
+
+        private static void scheduleCooldownRemoval(Player player) {
+            MinecraftServer.getSchedulerManager().buildTask(() ->
+                    removeCooldown(player))
+                    .delay(10, TimeUnit.SERVER_TICK).schedule();
+        }
     }
 }
