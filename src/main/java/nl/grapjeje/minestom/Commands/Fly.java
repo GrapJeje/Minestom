bcbdd304e3ea720117b9ad47ab66728b4ff70559
Jason <jason@camonetwork.eu>
2025-03-06 16:24:12 +0100

    Added a gamemode command

--- src/main/java/nl/grapjeje/minestom/Commands/Fly.java
+++ src/main/java/nl/grapjeje/minestom/Commands/Fly.java
@@ -1,20 +1,16 @@
 package nl.grapjeje.minestom.Commands;
 
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import net.minestom.server.color.Color;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Player;
 import nl.grapjeje.minestom.Util.Text;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 
 public class Fly extends Command {
 
     public Fly() {
         super("fly");
 
-        setDefaultExecutor((sender, context) -> {
+        this.setDefaultExecutor((sender, context) -> {
             Player player = (Player) sender;
 
             if (player.isAllowFlying()) {
@@ -29,4 +25,3 @@ public class Fly extends Command {
         });
     }
 }
-
---
+++ src/main/java/nl/grapjeje/minestom/Commands/Gamemode.java
@@ -0,0 +1,43 @@
+package nl.grapjeje.minestom.Commands;
+
+import net.kyori.adventure.text.format.TextColor;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.entity.GameMode;
+import net.minestom.server.entity.Player;
+import nl.grapjeje.minestom.Util.Text;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Gamemode extends Command {
+    public Gamemode() {
+        super("gamemode", "gm");
+
+        var gamemodeArg = ArgumentType.String("gamemode");
+
+        this.setDefaultExecutor((sender, context) ->
+                sender.sendMessage(Text.getColoredMessage(TextColor.fromHexString("#1FC077"), "Da kan nie")));
+
+        this.addSyntax((sender, context) -> {
+            if (!(sender instanceof Player player)) return;
+
+            String mode = context.get(gamemodeArg);
+            GameMode gameMode = switch (mode.toLowerCase()) {
+                case "creative", "1" -> GameMode.CREATIVE;
+                case "survival", "0" -> GameMode.SURVIVAL;
+                case "adventure", "2" -> GameMode.ADVENTURE;
+                case "spectator", "3" -> GameMode.SPECTATOR;
+                default -> null;
+            };
+
+            if (gameMode == null) {
+                player.sendMessage(Text.getColoredMessage(TextColor.fromHexString("#1FC077"), "Da kan nie"));
+                return;
+            }
+
+            player.setGameMode(gameMode);
+            player.sendMessage(Text.getColoredMessage(TextColor.fromHexString("#1FC077"), "Je gamemode is veranderd naar ")
+                    .append(Text.getColoredMessage(TextColor.fromHexString("#D48341"), mode.toLowerCase())));
+        }, gamemodeArg);
+    }
+}
